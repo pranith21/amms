b6b7e76c723a8db3845e00571cca48587952af98
pranith21 <pranithkumarp@gmail.com>
2014-02-05 18:57:55 +0530

    Jan Checkins

--- iq-amms/src/org/iq/startup/StartupServlet.java
+++ iq-amms/src/org/iq/startup/StartupServlet.java
@@ -1,6 +1,9 @@
 package org.iq.startup;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletContext;
@@ -8,7 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import com.iq.amms.runners.BillGenerator;
+import com.iq.amms.services.helpers.BillHelper;
+import com.iq.amms.services.helpers.SystemHelper;
 
 /**
  * 
@@ -47,6 +51,9 @@ public class StartupServlet extends GenericServlet {
     
     super.init();
     System.out.println("APPLICATION STARTED SUCCESSFULLY");
+    
+    generateBill();
+    
   }
 
   /**
@@ -54,6 +61,29 @@ public class StartupServlet extends GenericServlet {
    */
   private void initializeServiceModule() {
     System.out.println("SERVICE MODULE INITIALIZED");
+  }
+  
+  private void generateBill(){
+	  SystemHelper systemHelper = new SystemHelper();
+	  try {
+		  Date startDate = new Date(systemHelper.getLastBillDate());
+		  Date endDate = new Date();
+		  Calendar startCalendar = new GregorianCalendar();
+		  startCalendar.setTime(startDate);
+		  Calendar endCalendar = new GregorianCalendar();
+		  endCalendar.setTime(endDate);
+
+		  int diffYear = endCalendar.get(Calendar.YEAR) - startCalendar.get(Calendar.YEAR);
+		  int diffMonth = diffYear * 12 + endCalendar.get(Calendar.MONTH) - startCalendar.get(Calendar.MONTH);		  
+		  BillHelper billHelper = new BillHelper();
+		  for (int i = 1; i < diffMonth; i++) {
+			  startCalendar.set(Calendar.MONTH, startCalendar.get(Calendar.MONTH)+i);
+			  billHelper.generateInvoices(startCalendar.getTime());
+		}
+	  } catch (Exception e) {
+		  System.out.println("Error Generating Bill....");
+		  e.printStackTrace();
+	  }
   }
 
   /*
